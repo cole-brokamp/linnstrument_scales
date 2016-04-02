5dd96c6347e320e37ab30bf42a2baec8b08ec25c
Cole Brokamp <cole.brokamp@gmail.com>
2016-04-02 12:12:51 -0400

    redo layout and add contact info

--- ui.R
+++ ui.R
@@ -2,18 +2,30 @@
 
 library(shiny)
 
-shinyUI(pageWithSidebar(
-  headerPanel('Linnstrument Scales'),
-  sidebarPanel(
-    selectInput('root.note', 'Root Note', names(note.numbers),'Major'),
-    selectInput('musical.mode', 'Mode', names(musical.modes),'C'),
-    sliderInput('zoom.factor', 'Circle Zoom', 1,
-                 min = 1, max = 3, step = 0.1),
-    checkboxInput('key.colors','Color Code Chord Types?',value=FALSE),
-    plotOutput('chord_legend',width='200%',height='800px')
-  ),
-  mainPanel(
-    plotOutput('linnplot_small',width='50%',height='200px')
-  )
+shinyUI(fluidPage(
+  
+  title = "Linnstrument Scale Explorer",
+  
+  plotOutput('linnplot_small',width='100%',height='500px'),
+  
+  hr(),
+  
+  fluidRow(
+    column(3,
+           h4('Scale Settings'),
+           selectInput('root.note', 'Root Note', names(note.numbers),'Major'),
+           br(),
+           selectInput('musical.mode', 'Mode', names(musical.modes),'C')
+    ),
+    column(4, offset = 1,
+           h4('Visual Settings'),
+           sliderInput('zoom.factor', 'Circle Zoom', 1.5, min = 0.5, max = 3, step = 0.1),
+           checkboxInput('key.colors','Color Code Chord Types?',value=FALSE),
+           imageOutput('chord_legend')
+    ),
+    column(2,
+           br(),
+           h4('Linnstrument Scale Explorer'),
+           h5('Questions/Comments: cole.brokamp@gmail.com')
+  ))
 ))
-
